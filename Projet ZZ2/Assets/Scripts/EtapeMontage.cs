a50b859fd104c4776007afe25b953e151d2950bc
HugoBarrandon <hugobarrandon@gmail.com>
2022-12-02 19:09:31 +0100

    Ajout prefab

---
+++ Projet ZZ2/Assets/Scripts/EtapeMontage.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EtapeMontage : MonoBehaviour
+{
+    public Movable _aDeplacer;
+    public Movable _destination;
+}
---
+++ Projet ZZ2/Assets/Scripts/MountableRoot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountableRoot : MonoBehaviour
+{
+    public List<Movable> pieces = new List<Movable>();
+    public List<EtapeMontage> lesEtapes = new List<EtapeMontage>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
